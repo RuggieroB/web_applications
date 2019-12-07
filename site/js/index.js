9110dae759db71d529e4af371d3f8222fe0dd915
RuggieroB <1bmeccenricorugbucchianico@gmail.com>
2019-12-07 09:35:18 +0100

    web_application
    Signed-off-by: RuggieroB <1bmeccenricorugbucchianico@gmail.com>

---
+++ site/js/index.js
@@ -0,0 +1,35 @@
+/*
+
+	# web_applications
+
+		web_applications - TPSIT A.S.: 2019/2020 5^Finf.
+	
+	
+			Utilizzo di JavaScript
+			
+*/
+
+const express=require('express');
+
+const log=require('morgan');
+
+const app=express();
+
+const path=require('path');
+
+let counter=0;
+
+app.listen(3000);
+
+app.use(log('dev'));
+
+app.use(function(req,res,next){
+	counter++;
+	console.log('Counter: ',counter);
+	next();});
+
+app.get('/',function(req,res){
+	res.sendfile(path.join(__dirname,'public','index.html'));});	
+	
+app.use(function(req,res){
+	res.sendfile(path.join(__dirname,'public','404.html'));});
